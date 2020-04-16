f45e799d79cb1e531ef9782ec8405678980c0234
Yi HAN <hanyi@8110@gmail.com>
2020-04-16 14:25:46 +0200

    Init server

---
+++ Controllers/QuestionsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QuestionsController : ControllerBase
+    {
+        [HttpPost]
+        public string Accept(Question question)
+        {
+            return question.Text;
+        }
+
+    }
+
+    public class Question
+    {
+        public string Text { get; set; }
+    }
+}
