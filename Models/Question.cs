316470bcf57d964c6dd928731e94a76cf1eea233
Yi HAN <hanyi@8110@gmail.com>
2020-04-17 14:22:43 +0200

    Add quizId as property to Question

--- Models/Question.cs
+++ Models/Question.cs
@@ -18,5 +18,7 @@
         public string Answer1 { get; set; }
         public string Answer2 { get; set; }
         public string Answer3 { get; set; }
+
+        public int QuizId { get; set; }
     }
 }
